e85007bc7c1c76af41810b8c1d8b13a9b87f8f17
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-10-05 19:49:31 -0700

    Make header responsive and update button styling

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,7 +1,7 @@
-export default function Button({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <button className="bg-primary font-bold py-2.5 px-4 rounded-full">{children}</button>;
+export default function Button({ children }: { children: React.ReactNode }) {
+  return (
+    <button className="bg-primary font-bold py-2.5 px-5 rounded-full">
+      {children}
+    </button>
+  );
 }
