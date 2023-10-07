73f333e186076281f9d5fb64b7adcbfa2960cec9
Rogelio Camargo <rogelio.camargo2016@gmail.com>
2023-10-07 14:11:29 -0700

    Complete styling taskdetails components and it's children

--- components/ui/SubTask.tsx
+++ components/ui/SubTask.tsx
@@ -2,9 +2,15 @@ import { SubTask } from "@/types";
 
 export default function SubTask({ subtask }: { subtask: SubTask }) {
   return (
-    <li className="bg-gray-600 px-3 py-4">
+    <li className="bg-gray-100 dark:bg-gray-600 px-3 py-4">
       <label className="container flex items-center gap-4">
-        <span className={[subtask.isCompleted ? "line-through text-gray-300" : ""].join(" ")}>
+        <span
+          className={[
+            subtask.isCompleted
+              ? "line-through text-gray-300"
+              : "text-black dark:text-white",
+          ].join(" ")}
+        >
           {subtask.title}
         </span>
         <input
@@ -12,7 +18,7 @@ export default function SubTask({ subtask }: { subtask: SubTask }) {
           checked={subtask.isCompleted}
           onChange={() => console.log("update checkbox")}
         />
-        <span className="checkmark"></span>
+        <span className="checkmark bg-white dark:bg-gray-500 border-gray-200 dark:border-gray-400 border"></span>
       </label>
     </li>
   );
--- components/ui/TaskDetails.tsx
+++ components/ui/TaskDetails.tsx
@@ -20,9 +20,9 @@ export default function TaskDetails({
         className="h-screen w-screen absolute top-0 left-0 bg-black bg-opacity-50 z-0"
         onClick={closeDetails}
       />
-      <div className="text-xs bg-gray-500 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-[480px] mx-auto rounded-md flex flex-col gap-6 p-6">
+      <div className="text-xs font-bold text-gray-300 dark:text-white bg-white dark:bg-gray-500 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-11/12 md:w-[480px] mx-auto rounded-md flex flex-col gap-6 p-6">
         <div className="flex justify-between items-center gap-8">
-          <h3 className="text-lg font-bold">{task.title}</h3>
+          <h3 className="text-lg font-bold text-black dark:text-white">{task.title}</h3>
           <svg width="7" height="22" xmlns="http://www.w3.org/2000/svg">
             <g fill="#828FA3" fill-rule="evenodd">
               <circle cx="2.308" cy="2.308" r="2.308" />
@@ -46,9 +46,9 @@ export default function TaskDetails({
         </div>
         <label className="flex flex-col gap-2">
           Current Status
-          <div className="custom-select bg-gray-500 border-gray-400 border-2 rounded-sm">
+          <div className="custom-select bg-white dark:bg-gray-500 border-gray-200 dark:border-gray-400 border rounded-sm">
             <select
-              className="px-4 py-2"
+              className="px-4 py-2 font-normal"
               value={task.status}
               onChange={() => console.log("selected")}
             >
